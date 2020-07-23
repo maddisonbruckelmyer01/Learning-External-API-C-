1125de8f915e1332d91b291271ff181910102e3e
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-23 15:48:32 -0500

    adds basic logic for sun model

---
+++ DemoLibrary/SunModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SunModel
+    {
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+    }
+}
