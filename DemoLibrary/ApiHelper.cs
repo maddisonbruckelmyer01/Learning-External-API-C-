7f6a13fdf0cbfe004a06a7e09120b2cd570d471d
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-23 14:58:22 -0500

    Creates InitializeClient method

--- DemoLibrary/ApiHelper.cs
+++ DemoLibrary/ApiHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,13 @@ namespace DemoLibrary
 {
     public class ApiHelper
     {
-        public static HttpClient ApiClient { get; set; } = new HttpClient();
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
