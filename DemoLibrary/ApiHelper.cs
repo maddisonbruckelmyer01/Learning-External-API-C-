3e1d7a9960c95d4e8c96be26ee57b585106be611
Maddison Bruckelmyer <maddison.bruckelmyer@reviirdevelopment.com>
2020-07-23 14:50:32 -0500

    Add project files.

---
+++ DemoLibrary/ApiHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+    }
+}
---
+++ WPFApp/SunInfo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for SunInfo.xaml
+    /// </summary>
+    public partial class SunInfo : Window
+    {
+        public SunInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
